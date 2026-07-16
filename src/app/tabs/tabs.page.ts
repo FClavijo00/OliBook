215463af71d705f3cc26aaecc93a4e66fbb3380e
FClavijo00 <fjclavijo00@gmail.com>
2026-07-16 21:16:38 +0200

    Pequeños cambios visuales

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, home, homeOutline, layers, layersOutline, checkmarkCircle, checkmarkCircleOutline, location, locationOutline, personCircle, personCircleOutline } from 'ionicons/icons';
-import { ModalController, IonicModule, ToastController } from '@ionic/angular';
+import { home, homeOutline, checkmarkCircle, checkmarkCircleOutline, personCircle, personCircleOutline, leaf, leafOutline } from 'ionicons/icons';
+import { IonicModule } from '@ionic/angular';
 
 
 @Component({
@@ -15,7 +15,7 @@ export class TabsPage {
   currentTab: string = 'home';
 
   constructor() {
-    addIcons({ triangle, ellipse, square, home, homeOutline, layers, layersOutline, checkmarkCircle, checkmarkCircleOutline, location, locationOutline, personCircle, personCircleOutline });
+    addIcons({ home, homeOutline, leaf, leafOutline, checkmarkCircle, checkmarkCircleOutline, personCircle, personCircleOutline });
   }
 
   setCurrentTab(event: any) {
