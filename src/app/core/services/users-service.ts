5d929d4b3d9bf6dd37e3daca650537700d216a18
FClavijo00 <fjclavijo00@gmail.com>
2026-07-24 20:17:44 +0200

    login, register y getUser en servicio

--- src/app/core/services/users-service.ts
+++ src/app/core/services/users-service.ts
@@ -1,18 +1,68 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../models/user';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { User, UserLogin, UserRegister } from '../models/user';
+import { Router } from '@angular/router';
+
+export interface LoginResponse {
+  message: string;
+  user: User;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
 
-  private apiUrl = 'http://localhost:3000/api/test';
+  public currentUser = signal<User | null>(this.getUserFromStorage());
+
+  private _apiUrl = 'http://localhost:3000/api/test';
   
   private _http = inject(HttpClient);
+  private _router = inject(Router);
+
+  login(loginData: UserLogin): Observable<LoginResponse> {
+    return this._http.post<LoginResponse>(`${this._apiUrl}/login`, loginData).pipe(
+      tap((response) => {
+        console.log('Respuesta del login:', response);
+        if (response && response.user) {
+          console.log('Usuario logueado:', response.user);
+          // Guardamos el usuario en el estado y en LocalStorage
+          this.setCurrentUser(response.user);
+        }
+      })
+    );
+  }
+
+
+  register(user: UserRegister): Observable<LoginResponse> {
+    return this._http.post<LoginResponse>(`${this._apiUrl}/register`, user);
+  }
 
   getUsers() : Observable<User[]> {
-    return this._http.get<User[]>(`${this.apiUrl}/getUsers`);
+    return this._http.get<User[]>(`${this._apiUrl}/getUsers`);
+  }
+
+  // Establece el usuario en memoria y almacenamiento local
+  setCurrentUser(user: User) {
+    this.currentUser.set(user);
+    localStorage.setItem('olibook_user', JSON.stringify(user));
+  }
+
+  // Obtiene el usuario actual
+  getUser(): User | null {
+    return this.currentUser();
+  }
+
+  // Para cerrar sesión
+  logout() {
+    this.currentUser.set(null);
+    localStorage.removeItem('olibook_user');
+    this._router.navigate(['/login']);
+  }
+
+  private getUserFromStorage(): User | null {
+    const savedUser = localStorage.getItem('olibook_user');
+    return savedUser ? JSON.parse(savedUser) : null;
   }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonicModule } from "@ionic/angular";
+import { Router } from '@angular/router';
+import { IonicModule, ToastController } from "@ionic/angular";
 import { addIcons } from 'ionicons';
-import { businessOutline, cameraOutline, eyeOffOutline, eyeOutline, leaf, lockClosedOutline, logInOutline, mailOutline, personAddOutline, personOutline } from 'ionicons/icons';
+import { alertCircleOutline, businessOutline, cameraOutline, checkmarkCircleOutline, eyeOffOutline, eyeOutline, informationCircle, leaf, lockClosedOutline, logInOutline, mailOutline, personAddOutline, personOutline } from 'ionicons/icons';
+import { firstValueFrom } from 'rxjs';
+import { User } from 'src/app/core/models/user';
+import { ToastService } from 'src/app/core/services/toast-service';
+import { UsersService } from 'src/app/core/services/users-service';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +22,11 @@ export class LoginPage implements OnInit {
   showPassword: boolean = false;
   isEnterprise: boolean = false;
   avatarPreview: string | null = null;
+  loadingSpinner: boolean = false;
+
+  private _userService = inject(UsersService);
+  private _toastService = inject(ToastService);
+  private _router = inject(Router);
 
   constructor(private _fb: FormBuilder) {
     addIcons({
@@ -28,7 +39,10 @@ export class LoginPage implements OnInit {
       personAddOutline,
       logInOutline,
       businessOutline,
-      cameraOutline
+      cameraOutline,
+      informationCircle,
+      checkmarkCircleOutline,
+      alertCircleOutline
     })
   }
 
@@ -41,27 +55,75 @@ export class LoginPage implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       // Campos de Registro
+      tipoUsuario: ['PARTICULAR'],
       nombre: [''],
       fotoUrl: [''],
-      isEmpresa: [false],
-      nombreEmpresa: ['']
+      nombreEmpresa: [''],
+      codigoEmpresa: ['']
     });
   }
 
   toggleMode() {
     this.isRegister = !this.isRegister;
     const nombreControl = this.authForm.get('nombre');
+    const empresaControl = this.authForm.get('nombreEmpresa');
+    const codigoControl = this.authForm.get('codigoEmpresa');
+    const tipoControl = this.authForm.get('tipoUsuario');
+    const passControl = this.authForm.get('password');
 
     if (this.isRegister) {
       // Si pasa a registro, el nombre es obligatorio
       nombreControl?.setValidators([Validators.required, Validators.minLength(2)]);
+      passControl?.setValue('');
     } else {
       // Si vuelve a login, limpiamos el campo y sus validaciones
       nombreControl?.clearValidators();
       nombreControl?.setValue('');
+
+      empresaControl?.clearValidators();
+      empresaControl?.setValue('');
+
+      codigoControl?.clearValidators();
+      codigoControl?.setValue('');
+
+      tipoControl?.setValue('PARTICULAR');
+
+      passControl?.setValue('');
     }
     
     nombreControl?.updateValueAndValidity();
+    empresaControl?.updateValueAndValidity();
+    codigoControl?.updateValueAndValidity();
+    tipoControl?.updateValueAndValidity();
+  }
+
+  changeTipoUsuario() {
+    const tipoControl = this.authForm.get('tipoUsuario');
+    const empresaControl = this.authForm.get('nombreEmpresa');
+    const codigoControl = this.authForm.get('codigoEmpresa');
+    if (tipoControl?.value === 'EMPRESA') {
+      this.isEnterprise = true;
+      empresaControl?.setValidators([Validators.required, Validators.minLength(2)]);
+      empresaControl?.updateValueAndValidity();
+      codigoControl?.clearValidators();
+      codigoControl?.setValue(null);
+      codigoControl?.updateValueAndValidity();
+    } else if (tipoControl?.value === 'TRABAJADOR') {
+      this.isEnterprise = false;
+      empresaControl?.clearValidators();
+      empresaControl?.setValue(null);
+      empresaControl?.updateValueAndValidity();
+      codigoControl?.setValidators([Validators.required, Validators.minLength(2)]);
+      codigoControl?.updateValueAndValidity();
+    } else if (tipoControl?.value === 'PARTICULAR') {
+      this.isEnterprise = false;
+      empresaControl?.clearValidators();
+      empresaControl?.setValue(null);
+      empresaControl?.updateValueAndValidity();
+      codigoControl?.clearValidators();
+      codigoControl?.setValue(null);
+      codigoControl?.updateValueAndValidity();
+    }
   }
 
   // Manejador para seleccionar o subir la foto de perfil
@@ -77,17 +139,64 @@ export class LoginPage implements OnInit {
     }
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.authForm.invalid) return;
 
+    this.loadingSpinner = true;
     const credentials = this.authForm.value;
     
     if (this.isRegister) {
       console.log('Registrando usuario:', credentials);
-      // Aquí irá la llamada a: this._authService.register(credentials)
+      try {
+        const response = await firstValueFrom(this._userService.register(credentials));
+        if (response) {
+          this._toastService.presentToast(
+            'Usuario registrado correctamente.',
+            'toast-success',
+            'checkmark-circle-outline'
+          )
+        }
+        this.toggleMode();
+      } catch (error: HttpErrorResponse | any) {
+        if (error.status === 400) {
+          this._toastService.presentToast(
+            'El código de empresa proporcionado no es correcto.',
+            'toast-error',
+            'alert-circle-outline'
+          )
+        }
+      } finally {
+        this.loadingSpinner = false;
+      }
     } else {
-      console.log('Iniciando sesión con:', { email: credentials.email, password: credentials.password });
-      // Aquí irá la llamada a: this._authService.login(credentials)
+      let loginData = { email: credentials.email, password: credentials.password };
+      try {
+        const response = await firstValueFrom(this._userService.login(loginData));
+        if (response) {
+          this._toastService.presentToast(
+            'Inicio de sesión realizado con éxito.',
+            'toast-success',
+            'checkmark-circle-outline'
+          );
+          this._router.navigate(['/tabs/home']);
+        }
+      } catch (error: HttpErrorResponse | any) {
+        if (error.status === 401) {
+          this._toastService.presentToast(
+            'Usuario o contraseña incorrectos. Pruebe de nuevo.',
+            'toast-error',
+            'alert-circle-outline'
+          )
+        } else {
+          this._toastService.presentToast(
+            'Ha ocurrido un error inesperado. Pruebe de nuevo.',
+            'toast-error',
+            'alert-circle-outline'
+          )
+        }
+      } finally {
+        this.loadingSpinner = false;
+      }
     }
   }
 }
