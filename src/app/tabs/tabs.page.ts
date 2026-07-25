21ddbf2ce6cb1b21fdc58258680a418f2e9c89f5
FClavijo00 <fjclavijo00@gmail.com>
2026-07-25 21:46:18 +0200

    Cambio de imports en tabs.page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,14 +1,15 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { addIcons } from 'ionicons';
 import { home, homeOutline, checkmarkCircle, checkmarkCircleOutline, personCircle, personCircleOutline, leaf, leafOutline } from 'ionicons/icons';
-import { IonicModule } from '@ionic/angular';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonicModule],
+  imports: [IonIcon, IonTabButton, IonTabBar, IonTabs, ],
+  standalone: true
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
